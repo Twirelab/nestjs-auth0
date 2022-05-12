7dec39ebedaebe25de2365c4fba3b847493f92f1
Lukasz Lupa <lukasz.lupa@gmail.com>
2022-05-12 10:54:00 +0100

    Try fix problem with import package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./auth0.options";
 export * from "./inject/authentication.inject";
 export * from "./inject/management.inject";
-export * from "./modules/authentication-core.module";
-export * from "./modules/management-core.module";
+export * from "./modules/authentication.module";
+export * from "./modules/management.module";
